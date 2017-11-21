8c18f9fd96f96e1c2eb5a480381960158b50f6c0
dixinluo <dxluo611@gmail.com>
2017-11-21 00:28:24 -0500

    Create Smallest_Rectangle_Enclosing_Black_Pixels.py

---
+++ Smallest_Rectangle_Enclosing_Black_Pixels.py
@@ -0,0 +1,9 @@
+"""
+An image is represented by a binary matrix with 0 as a white pixel and 1 as a black pixel. 
+The black pixels are connected, i.e., there is only one black region. 
+Pixels are connected horizontally and vertically. 
+Given the location (x, y) of one of the black pixels, 
+return the area of the smallest (axis-aligned) rectangle that encloses all black pixels.
+
+# https://discuss.leetcode.com/topic/29086/clear-binary-search-python
+"""
