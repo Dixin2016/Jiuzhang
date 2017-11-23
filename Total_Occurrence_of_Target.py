161d689e7f70a21ca9e9e625dd88b7e902588014
dixinluo <dxluo611@gmail.com>
2017-11-22 19:17:13 -0500

    Create Total_Occurrence_of_Target.py

---
+++ Total_Occurrence_of_Target.py
@@ -0,0 +1,19 @@
+"""
+LintCode 462 - Total Occurrence of Target
+
+Given a target number and an integer array sorted in ascending order. 
+Find the total number of occurrences of target in the array.
+
+Example 
+Given [1, 3, 3, 4, 5] and target = 3, return 2.
+Given [2, 2, 3, 4, 6] and target = 4, return 1.
+Given [1, 2, 3, 4, 5] and target = 6, return 0.
+
+Time complexity in O(logn)
+
+Binary Search
+"""
+
+class Solution(object):
+    def totalOccurrence(self, nums, target):
+        if 
