9f9586150796bd760dcb643962fa8c81c18bb4ac
dixinluo <dxluo611@gmail.com>
2017-11-25 23:36:53 -0500

    Update K_Closest_Numbers_in_Sorted_Array.py

--- K_Closest_Numbers_in_Sorted_Array.py
+++ K_Closest_Numbers_in_Sorted_Array.py
@@ -13,3 +13,30 @@ Challenge
 O(logn + k) time complexity
 """
 class Solution:
+    def kClosestNumbers(self, nums, target, k):
+        if not nums or len(nums) == 0 or k == 0:
+            return []
+        
+        if target <= nums[0]:
+            return nums[0 : k]
+        if target >= nums[-1]:
+            return nums[-k:]
+        
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if nums[mid] >= target:
+                end = mid
+            else:
+                start = mid
+        
+        res = []
+        while k > 0 and (start >= 0 or end < len(nums)):
+            if (start >=0 and end < len(nums) and abs(nums[start] - target) <= abs(nums[end] - target)) or end >= len(nums):
+                res.append(nums[start])
+                start -= 1
+            elif (start >=0 and end < len(nums) and abs(nums[start] - target) > abs(nums[end] - target)) or start < 0:
+                res.append(nums[end])
+                end += 1
+            k -= 1
+        return res
